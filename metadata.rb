3b55de46761c3289a2da7a3f47ebe5b13c7ea14e
Léo Goffic <leo.goffic@gmail.com>
2014-05-28 13:02:12 +0200

    Add name metadata

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license          "Apache 2.0"
 description      "Deploys and configures Node.js applications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "2.0.0"
+name             "application_nodejs"
 
 depends          "nodejs"
 depends          "application"
